a9e8e3b7696a3cb2c480234fc156594a4e92ac70
aprilslis <s826li@uwaterloo.ca>
2024-08-18 13:44:14 -0700

    idk

--- firestore_init.py
+++ firestore_init.py
@@ -1,27 +1,3 @@
-# import firebase_admin
-# from firebase_admin import credentials, firestore
-# import os
-# from dotenv import load_dotenv
-# import json
-
-# load_dotenv()
-
-# # cred = credentials.Certificate('./serviceAccount.json')
-# cred = os.getenv("CREDS")
-# if not cred:
-#     raise ValueError('The $CREDS environment variable was not found!')
-
-# # Parse the JSON credentials from the environment variable
-# creds_info = json.loads(cred)
-
-# # Load the credentials using the service account
-# credentials = credentials.Certificate(creds_info)
-
-
-# app = firebase_admin.initialize_app(cred)
-# db = firestore.client()
-
-
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
